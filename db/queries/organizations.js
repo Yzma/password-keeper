657d2389b43c2311327be1bb66e4798f96f770af
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-02-11 11:42:45 -0500

    Implement getOrganizations

---
+++ db/queries/organizations.js
@@ -0,0 +1,13 @@
+
+const db = require('../connection');
+
+const getOrganizations = () => {
+  return db.query('SELECT * FROM organizations;')
+    .then(data => {
+      return data.rows;
+    });
+};
+
+module.exports = {
+  getOrganizations
+};
