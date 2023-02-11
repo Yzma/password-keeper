2edd709ea2cd2214a2daedeb6bf1cf5b9198fbdb
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-02-10 22:48:05 -0500

    Implemenet a basic login route

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,33 @@ const express = require('express');
 const router  = express.Router();
 
 const users = require('../db/queries/users');
+const bcrypt = require('bcryptjs');
 
-router.post('/login', (req, res) => {
-  res.send();
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  // TODO: Create util methods to check
+
+  // if (!isValid(email, password)) {
+  //   return res.status(400).json({ error: 'Invalid username or password'});
+  // }
+
+  // TODO:
+  // Cleanup
+  // Figure out if we want to use bcrypt here or somewhere else (user helper, utils)
+  return users.getUserByEmail(email)
+    .then(result => result[0]) // TODO: Change this so the result isn't an array of rows
+    .then(user => {
+      if (bcrypt.compare(password, user.password)) {
+        req.session.userID = user.id;
+        return res.redirect('/urls');
+      }
+
+      return res.json({ error: 'Invalid credentials' });
+    }).catch((err) => {
+      console.log('Error logging in: ', err);
+      return res.send('Invalid login');
+    });
 });
 
 router.post('/logout', (req, res) => {
