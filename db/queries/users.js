d50573bd86654bac35a4a4be86182719e0888c48
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-02-10 19:02:05 -0500

    Added empty skeleton functions for db users helper functions

--- db/queries/users.js
+++ db/queries/users.js
@@ -7,4 +7,24 @@ const getUsers = () => {
     });
 };
 
+const getUserByUsername = (username) => {
+  return db.query('SELECT * FROM users WHERE users.username = $1;', [username])
+    .then(data => {
+      return data.rows;
+    });
+};
+
+const getUsersPasswordsById = (userId) => {
+  // TODO: Implement me
+};
+
+const getUsersOrganizationsById = (userId) => {
+  // TODO: Implement me
+};
+
+const getUsersPendingInvitesById = (userId) => {
+  // TODO: Implement me
+};
+
+
 module.exports = { getUsers };
