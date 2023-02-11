b5fa0f50e05ee9c222ddfccff346f0e9c4b625f4
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-02-10 22:15:28 -0500

    Implement /auth/me route to get user information about the logged in user

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router  = express.Router();
 
+const users = require('../db/queries/users');
+
 router.post('/login', (req, res) => {
   res.send();
 });
@@ -13,7 +15,17 @@ router.post('/logout', (req, res) => {
 
 // Debug route - Quickly get user information depending on the users cookie
 router.get('/me', (req, res) => {
-  res.send();
+  const userId = req.session.id;
+  if (!userId) {
+    return res.send('You are not logged in.');
+  }
+
+  return users._getMyInfo(userId)
+    .then(result => res.send(result))
+    .catch(err => {
+      console.log('Error fetching my info: ', err);
+      return res.send('Error fetching user data. Check console for error.');
+    });
 });
 
 module.exports = router;
