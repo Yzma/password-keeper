c61e07295f37e58310e68c20178c3a7f41a35f45
mauroapjr <mauroapjr@outlook.com>
2023-02-11 19:12:11 -0500

    add new password page

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,37 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('users');
+router.get("/", (req, res) => {
+  res.render("users");
+});
+
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.post("/login", (req, res) => {
+  console.log("Got body:", req.body);
+  res.sendStatus(200);
+});
+
+router.post("/register", (req, res) => {
+  console.log("Got body:", req.body);
+  res.sendStatus(200);
+});
+
+
+// get to create a new password
+router.get("/new_password", (req, res) => {
+  res.render("new_password");
+});
+// post to create a new password
+router.post("/new_password", (req, res) => {
+  console.log("Got body:", req.body);
+  res.sendStatus(200);
 });
 
 module.exports = router;
-
-
