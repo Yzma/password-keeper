677988983f6e188207386d927acdb8826452415a
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-02-13 16:46:41 -0500

    Split tags table into user_password_tags and organization_password_tags

--- db/seeds/01_basic_data.sql
+++ db/seeds/01_basic_data.sql
@@ -12,10 +12,13 @@ INSERT INTO organizations(owner_id, org_name) VALUES(1, 'Username 1s Org 2');
 INSERT INTO organizations(owner_id, org_name) VALUES(2, 'Username 2s Org');
 
 -- Tags
-INSERT INTO tags(name) VALUES('Finance');
-INSERT INTO tags(name) VALUES('Gaming');
-INSERT INTO tags(name) VALUES('Entertainment');
-INSERT INTO tags(name) VALUES('Social');
+INSERT INTO organization_password_tags(name, organization_id) VALUES('Finance', 1);
+INSERT INTO organization_password_tags(name, organization_id) VALUES('Gaming', 1);
+INSERT INTO organization_password_tags(name, organization_id) VALUES('Entertainment', 1);
+INSERT INTO organization_password_tags(name, organization_id) VALUES('Social', 1);
+
+INSERT INTO user_password_tags(name, user_id) VALUES('Finance', 2);
+INSERT INTO user_password_tags(name, user_id) VALUES('Gaming', 2);
 
 -- Insert some users into Organization 1
 INSERT INTO users_organizations(user_id, organization_id) VALUES(1, 1);
@@ -27,8 +30,8 @@ INSERT INTO user_passwords(website_name, username, password, user_id, tag_id) VA
 INSERT INTO user_passwords(website_name, username, password, user_id, tag_id) VALUES('user website 1', 'user username 1', 'test-password2', 1, 1);
 
 -- Org Passwords
-INSERT INTO organization_passwords(website_name, username, password, organization_id, tag_id) VALUES('org website 1', 'org username 1', 'org-test-password', 1, 3);
-INSERT INTO organization_passwords(website_name, username, password, organization_id, tag_id) VALUES('org website 1', 'org username 1', 'org-test-password2', 1, 3);
+INSERT INTO organization_passwords(website_name, username, password, organization_id, tag_id) VALUES('org website 1', 'org username 1', 'org-test-password', 1, 1);
+INSERT INTO organization_passwords(website_name, username, password, organization_id, tag_id) VALUES('org website 1', 'org username 1', 'org-test-password2', 1, 2);
 
 -- Invites
 INSERT INTO invites(user_id, organization_id) VALUES(4, 1);
