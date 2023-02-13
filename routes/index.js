6b1cd696be44251b0bc12d8565e028d495181b0a
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-02-13 13:01:13 -0500

    Use auth-middleware for frontend routes and pass req.user to render pages

--- routes/index.js
+++ routes/index.js
@@ -2,31 +2,36 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const authMiddleware = require('../lib/auth-middleware');
+
+// TODO: Change auth-middleware to redirect to login page on routes that are meant to be protected
+
+router.get('/', [authMiddleware()], (req, res) => {
   const templateVars = {
-    user: null
+    user: req.user
   };
   res.render('index', templateVars);
 });
 
-router.get("/users", (req, res) => {
+router.get("/users", [authMiddleware()], (req, res) => {
+  console.log('/users user: ', req.user);
   const templateVars = {
-    user: null
+    user: req.user
   };
   res.render("users", templateVars);
 });
 
-router.get("/login", (req, res) => {
+router.get("/login", [authMiddleware()], (req, res) => {
   const templateVars = {
-    user: null
+    user: req.user
   };
   res.render("login", templateVars);
 });
 
 // get to create a new password
-router.get("/new_password", (req, res) => {
+router.get("/new_password", [authMiddleware()], (req, res) => {
   const templateVars = {
-    user: null
+    user: req.user
   };
   res.render("new_password", templateVars);
 });
