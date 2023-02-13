dfd3f485a8b68d9a99ba029fb0490b134c0fdf7b
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-02-13 15:30:51 -0500

    Implement GET /organizations/:orgId/invites to get all organizations pending invites

--- routes/organizations/invites.js
+++ routes/organizations/invites.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router  = express.Router();
 
+const organizationHelper = require('../../db/queries/organizations');
+
 router.get("/:orgId/invites", (req, res) => {
-  return res.send('Should GET /organizations/{org_id}/invites');
+  const orgId = req.params.orgId;
+  
+  return organizationHelper.getOrganizationsPendingInvitesById(orgId)
+    .then(rows => res.json(rows))
+    .catch(err => res.json(err));
 });
 
 router.post('/:orgId/invites', (req, res) => {
