a9091ce6848cd4d73ca240a71d9b554788086aed
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-02-13 18:24:35 -0500

    Implement GET, POST, and DELETE for /organizations/:orgId/tags

--- routes/organizations/tags.js
+++ routes/organizations/tags.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const router  = express.Router();
 
+const organizationHelper = require('../../db/queries/organizations');
+
 router.get("/:orgId/tags", (req, res) => {
-  return res.send('Should GET /organizations/{org_id}/tags');
+  const orgId = req.params.orgId;
+  const { userId } = req.body;
+
+  return organizationHelper.getAllOrganizationTags(orgId)
+    .then(rows => res.json(rows))
+    .catch(err => res.json(err));
 });
 
 router.post('/:orgId/tags', (req, res) => {
-  return res.send('Should POST /organizations/{org_id}/tags');
+  const orgId = req.params.orgId;
+  const { name } = req.body;
+
+  return organizationHelper.createOrganizationTag(orgId, name)
+    .then(rows => res.json(rows))
+    .catch(err => res.json(err));
 });
 
 router.delete('/:orgId/tags', (req, res) => {
-  return res.send('Should DELETE /organizations/{org_id}/tags');
-});
+  const orgId = req.params.orgId;
+  const { name } = req.body;
 
+  return organizationHelper.deleteOrganizationTag(orgId, name)
+    .then(rows => res.json(rows))
+    .catch(err => res.json(err));
+});
 module.exports = router;
