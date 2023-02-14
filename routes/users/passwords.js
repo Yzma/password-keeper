fb3e54d938a58965c68941c5a217dced7550642a
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-02-14 10:40:12 -0500

    Push other user routes so project can build (WIP)

---
+++ routes/users/passwords.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router  = express.Router();
+
+const usersHelper = require('../../db/queries/users');
+
+router.get("/passwords", (req, res) => {
+  const userId = req.session.userID; // TODO: Use auth-middleware to handle this
+
+  return usersHelper.getUsersPasswordsById(userId)
+    .then(rows => res.json(rows))
+    .catch(err => res.json(err));
+});
+
+// router.post('/passwords', (req, res) => {
+//   const userId = req.session.userID; // TODO: Use auth-middleware to handle this
+
+//   return organizationHelper.insertPassword(orgId, websiteName, username, password, tagId)
+//     .then(rows => res.json(rows))
+//     .catch(err => res.json(err));
+// });
+
+// router.delete('/passwords', (req, res) => {
+//   const orgId = req.params.orgId;
+//   const { passwordId } = req.body;
+
+//   return organizationHelper.deletePassword(orgId, passwordId)
+//     .then(rows => res.json(rows))
+//     .catch(err => res.json(err));
+// });
+
+// router.patch('/passwords', (req, res) => {
+//   const orgId = req.params.orgId;
+//   const { passwordId, websiteName, username, password, tagId } = req.body;
+ 
+//   return organizationHelper.updatePassword(orgId, passwordId, websiteName, username, password, tagId)
+//     .then(rows => res.json(rows))
+//     .catch(err => {
+//       console.log('error: ', err);
+//       return res.json(err);
+//     });
+// });
+
+module.exports = router;
