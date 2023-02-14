7c03db48fa59db4c5460ec97343ec45c0f41550e
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-02-14 12:37:40 -0500

    Implement /users/passwords/ route

--- routes/users/passwords.js
+++ routes/users/passwords.js
@@ -11,33 +11,33 @@ router.get("/passwords", (req, res) => {
     .catch(err => res.json(err));
 });
 
-// router.post('/passwords', (req, res) => {
-//   const userId = req.session.userID; // TODO: Use auth-middleware to handle this
+router.post('/passwords', (req, res) => {
+  const userId = req.session.userID; // TODO: Use auth-middleware to handle this
 
-//   return organizationHelper.insertPassword(orgId, websiteName, username, password, tagId)
-//     .then(rows => res.json(rows))
-//     .catch(err => res.json(err));
-// });
+  return usersHelper.insertPassword(orgId, websiteName, username, password, tagId)
+    .then(rows => res.json(rows))
+    .catch(err => res.json(err));
+});
 
-// router.delete('/passwords', (req, res) => {
-//   const orgId = req.params.orgId;
-//   const { passwordId } = req.body;
+router.delete('/passwords', (req, res) => {
+  const orgId = req.params.orgId;
+  const { passwordId } = req.body;
 
-//   return organizationHelper.deletePassword(orgId, passwordId)
-//     .then(rows => res.json(rows))
-//     .catch(err => res.json(err));
-// });
+  return usersHelper.deletePassword(orgId, passwordId)
+    .then(rows => res.json(rows))
+    .catch(err => res.json(err));
+});
 
-// router.patch('/passwords', (req, res) => {
-//   const orgId = req.params.orgId;
-//   const { passwordId, websiteName, username, password, tagId } = req.body;
+router.patch('/passwords', (req, res) => {
+  const orgId = req.params.orgId;
+  const { passwordId, websiteName, username, password, tagId } = req.body;
  
-//   return organizationHelper.updatePassword(orgId, passwordId, websiteName, username, password, tagId)
-//     .then(rows => res.json(rows))
-//     .catch(err => {
-//       console.log('error: ', err);
-//       return res.json(err);
-//     });
-// });
+  return usersHelper.updatePassword(orgId, passwordId, websiteName, username, password, tagId)
+    .then(rows => res.json(rows))
+    .catch(err => {
+      console.log('error: ', err);
+      return res.json(err);
+    });
+});
 
 module.exports = router;
