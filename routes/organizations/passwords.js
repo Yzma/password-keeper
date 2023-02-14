96608eddf8a41baf3273f0d36ba5bf30d60de661
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-02-13 21:38:15 -0500

    Implement POST, DELETE, and PATCH routes for /organizations/:orgId/passwords

--- routes/organizations/passwords.js
+++ routes/organizations/passwords.js
@@ -13,15 +13,33 @@ router.get("/:orgId/passwords", (req, res) => {
 });
 
 router.post('/:orgId/passwords', (req, res) => {
-  return res.send('Should POST /organizations/{org_id}/passwords');
+  const orgId = req.params.orgId;
+  const { websiteName, username, password, tagId } = req.body;
+
+  return organizationHelper.insertPassword(orgId, websiteName, username, password, tagId)
+    .then(rows => res.json(rows))
+    .catch(err => res.json(err));
 });
 
 router.delete('/:orgId/passwords', (req, res) => {
-  return res.send('Should DELETE /organizations/{org_id}/passwords');
+  const orgId = req.params.orgId;
+  const { passwordId } = req.body;
+
+  return organizationHelper.deletePassword(orgId, passwordId)
+    .then(rows => res.json(rows))
+    .catch(err => res.json(err));
 });
 
 router.patch('/:orgId/passwords', (req, res) => {
-  return res.send('Should UPDATE /organizations/{org_id}/passwords');
+  const orgId = req.params.orgId;
+  const { passwordId, websiteName, username, password, tagId } = req.body;
+ 
+  return organizationHelper.updatePassword(orgId, passwordId, websiteName, username, password, tagId)
+    .then(rows => res.json(rows))
+    .catch(err => {
+      console.log('error: ', err);
+      return res.json(err);
+    });
 });
 
 module.exports = router;
