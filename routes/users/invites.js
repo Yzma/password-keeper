05c9ba2c05ce8b6a99d207e5d1eef0f9802e6c61
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-02-14 14:15:23 -0500

    Update users routes CRUD operations for user invites, passwords and tags

--- routes/users/invites.js
+++ routes/users/invites.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router  = express.Router();
 
 const userHelper = require('../../db/queries/users');
-const organizationHelper = require('../../db/queries/organizations');
 
 router.get("/invites", (req, res) => {
   const userId = req.session.userID; // TODO: Use auth-middleware to handle this
-  
+
   return userHelper.getUsersPendingInvitesById(userId)
     .then(rows => res.json(rows))
     .catch(err => res.json(err));
@@ -22,10 +21,10 @@ router.get("/invites", (req, res) => {
 // });
 
 router.delete('/invites', (req, res) => {
-  const orgId = req.params.orgId;
+  const userId = req.session.userID; // TODO: Use auth-middleware to handle this
   const { inviteId } = req.body;
 
-  return userHelper.deleteInvite(orgId, inviteId)
+  return userHelper.deleteInvite(userId, inviteId)
     .then(rows => res.json(rows))
     .catch(err => res.json(err));
 });
