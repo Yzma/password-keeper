c82ab83a1d891933113f4ea6655f786ade4b332f
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-02-14 00:48:11 -0500

    Implement index users route

---
+++ routes/users/index.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+const router  = express.Router();
+
+const usersHelper = require('../../db/queries/organizations');
+
+const invites = require('./invites');
+const passwords = require('./passwords');
+const tags = require('./tags');
+
+router.get("/", (req, res) => {
+  return usersHelper.getUsers()
+    .then(result => {
+      return res.json(result);
+    });
+});
+
+router.get("/:userId", (req, res) => {
+  const userId = req.params.userId;
+  return usersHelper.getUserById(userId)
+    .then(result => {
+      return res.json(result);
+    });
+});
+
+router.use('/', invites);
+router.use('/', passwords);
+router.use('/', tags);
+
+module.exports = router;
