51533d87be8579c75e73ee112046162acc8d899a
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-02-15 18:03:41 -0500

    Render organization pending invites on /orgs/invites

--- public/scripts/invites.js
+++ public/scripts/invites.js
@@ -1,3 +1,21 @@
+
+const fetchOrganizationOutgoingInvites = (org, callback) => {
+  $.get(`/organizations/${org.id}/invites`)
+    .then((result) => {
+      callback(null, result);
+    }).catch((e) => {
+      callback(e, null);
+    });
+};
+
+const renderInvite = (invite) => {
+  return `<div class="grid-item">
+    <a>ID: ${invite.id}</a>
+    <a>Created At: ${invite.created_at}</a>
+    <a>User ID: ${invite.user_id}</a>
+  </div>`;
+};
+
 $(document).ready(function() {
 
   const org = JSON.parse($("#org-json").text());
@@ -19,6 +37,24 @@ $(document).ready(function() {
         // TODO: Show HTML that the user was not found
         console.error('Error posting tweet:', e);
       });
+  });
+
+  fetchOrganizationOutgoingInvites(org, (error, result) => {
+    if (error) {
+      console.log('error loading pending invites', error);
+      return;
+    }
+
+    console.log('loaded: ', result);
+
+    const outgoingInvites = $("#outgoing-invites");
+    if (result.length > 0) {
+      outgoingInvites.removeClass('hidden');
+      for (let i of result) {
+        console.log(i);
+        outgoingInvites.append(renderInvite(i));
+      }
+    }
   });
 
   console.log(org);
